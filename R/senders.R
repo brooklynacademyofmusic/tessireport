9e89f9a216a091a27db435fc3c41a58836ef2783
Sky Syzygy <ssyzygy@bam.org>
2025-02-13 14:54:06 -0500

    Update send_email to use sendmailR and the curl backend for more flexible authentication

--- R/senders.R
+++ R/senders.R
@@ -1,37 +1,52 @@
 #' send_email
 #'
 #' @param emails email addresses (first will be sender)
-#' @inheritParams mailR::send.mail
-#' @param body Body of the email as text. If the parameter body refers to an existing file location,
-#' the text of the file is parsed as body of the email. Default is `Sent by <computer name>`.
-#' @inheritDotParams mailR::send.mail html attach.files
-#' @importFrom checkmate assert_character test_character test_list
-#' @importFrom mailR send.mail
+#' @param subject character subject of the email
+#' @param smtp named list, should contain `hostname` and (optionally) `port`, which defaults to 25.
+#'  Additional elements will get merged with `...` and passed to curl, see [curl::curl_options] for more details.
+#' @param body html of message or a list containing [sendmailR::mime_part] objects.
+#' @param attachments named vector, names are the attachment names, values are the filenames to attach.
+#' @importFrom checkmate assert_character test_character test_list assert check_character check_list
+#' @importFrom sendmailR sendmail mime_part mime_part_html
+#' @importFrom purrr keep_at imap
+#' @inheritParams sendmailR::sendmail
 #' @export
 send_email <- function(subject, body = paste("Sent by", Sys.info()["nodename"]),
                        emails = config::get("tessiflow.email"),
-                       smtp = config::get("tessiflow.smtp"),...
+                       smtp = config::get("tessiflow.smtp"),
+                       attachments = NULL, engine = "curl",
+                       ...
 ) {
   assert_character(subject, len = 1)
-  assert_character(body, len = 1)
+  assert(
+    check_character(body, len = 1),
+    check_list(body, "mime_part")
+  )
+  assert_character(attachments, null.ok = T)
 
   if (!test_character(emails, min.len = 1)) {
     stop("Set tessiflow.email to the sender (first email) and list of recipients for messages")
   }
   if (!test_list(smtp)) {
-    stop("Set tessiflow.smtp to the smtp server used to send messages")
+    stop("Set tessiflow.smtp to a list containing the `hostname` and optionally `port` of the smtp server")
   }
 
-  send.mail(
-    from = emails[[1]],
-    to = emails,
-    subject = subject,
-    body = body,
-    smtp = smtp,
-    encoding = "utf-8",
-    ...,
-    send = TRUE
-  )
+  if (typeof(body) == "character")
+    body <- mime_part_html(body)
+
+  if (length(attachments))
+    body <- c(body, imap(attachments,\(x,n) mime_part(x,n)))
+
+  dots <- modifyList(list(...),list(
+                     from = emails[[1]],
+                     to = emails,
+                     subject = subject,
+                     msg = body,
+                     control = list(smtpServer=smtp$hostname,smtpPort=smtp$port),
+                     engine = engine,
+                     engineopts = keep_at(smtp,names(curl::curl_options()))))
+
+  do.call(sendmail,dots)
 }
 
 #' send_xlsx
@@ -42,8 +57,7 @@ send_email <- function(subject, body = paste("Sent by", Sys.info()["nodename"]),
 #' @inheritParams write_xlsx
 #' @param table data.table to send
 #' @param basename name of the file to use in the email. Defaults to the name of the table.
-#' A timestamp and extension will be appended and passed on to `mailR::send.mail` as `file.names`
-#' @inheritDotParams mailR::send.mail html inline
+#' A timestamp and extension will be appended and passed on to [send_email] as the name of `attachments`.
 #' @inheritDotParams write_xlsx group currency
 #' @importFrom checkmate assert_data_table assert_character
 #' @export
@@ -56,12 +70,15 @@ send_xlsx <- function(table,
   assert_character(emails, min.len = 1)
 
   filename <- write_xlsx(table, ...)
+  name <- paste0(basename,"_",Sys.Date(),".xlsx")
 
-  send_email_args <- modifyList(list(subject = subject, body = body, emails = emails,
-                          html = TRUE, attach.files = filename,
-                          file.names = paste0(basename,"_",Sys.Date(),".xlsx")),
+  send_email_args <- modifyList(list(subject = subject,
+                                     body = body,
+                                     emails = emails,
+                                     attachments = setNames(filename,name)),
                      rlang::list2(...))
 
   do.call(send_email,send_email_args)
 
 }
+
