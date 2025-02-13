c530e06e7f7b5e09d1bf93d1fcf14a21a05250d4
Sky Syzygy <ssyzygy@bam.org>
2025-02-13 14:54:40 -0500

    Updated tests for new sendmailR

--- tests/testthat/test_senders.R
+++ tests/testthat/test_senders.R
@@ -5,8 +5,8 @@ withr::local_package("mockery")
 # send_email --------------------------------------------------------------
 
 test_that("send_email complains if subject, emails, or smtp are not set correctly", {
-  send.mail <- mock()
-  stub(send_email, "send.mail", send.mail)
+  sendmail <- mock()
+  stub(send_email, "sendmail", sendmail)
 
   expect_error(send_email(),"subject.+missing")
 
@@ -18,29 +18,38 @@ test_that("send_email complains if subject, emails, or smtp are not set correctl
   expect_error(send_email("subject", "body"),"smtp server")
 })
 
-test_that("send_email passes on parameters to send.mail", {
-  send.mail <- mock()
-  stub(send_email, "send.mail", send.mail)
-  stub(send_email, "config::get", mock("test@test.com", list(host.name = "blah")))
+test_that("send_email passes on parameters to sendmail", {
+  sendmail <- mock()
+  stub(send_email, "sendmail", sendmail)
+  stub(send_email, "config::get", mock("test@test.com", list(hostname = "blah")))
 
   expect_silent(send_email("subject","body"))
-  expect_length(mock_args(send.mail),1)
-  expect_equal(mock_args(send.mail)[[1]][["subject"]],"subject")
-  expect_equal(mock_args(send.mail)[[1]][["body"]],"body")
-  expect_equal(mock_args(send.mail)[[1]][["from"]],"test@test.com")
-  expect_equal(mock_args(send.mail)[[1]][["to"]],"test@test.com")
-  expect_equal(mock_args(send.mail)[[1]][["smtp"]],list(host.name = "blah"))
+  expect_length(mock_args(sendmail),1)
+  expect_match(mock_args(sendmail)[[1]]$subject,"subject")
+  expect_match(mock_args(sendmail)[[1]]$msg$text,"body")
+  expect_equal(mock_args(sendmail)[[1]]$from,"test@test.com")
+  expect_equal(mock_args(sendmail)[[1]]$to,"test@test.com")
+  expect_equal(mock_args(sendmail)[[1]]$control,list(smtpServer="blah",smtpPort=NULL))
 })
 
 test_that("send_email sends an email", {
-  stub(send.mail, ".jTryCatch", function(...) {
-    rlang::warn(class = "sent!")
-  })
-  stub(send_email, "send.mail", send.mail)
   stub(send_email, "config::get", mock("test@test.com", list(host.name = "blah"), cycle = TRUE))
-  expect_warning(send_email("subject", "body"), class = "sent!")
+  mail <- send_email(engine="debug","This is a test email")
+  expect_match(mail,"This is a test email",all = F)
+  expect_match(mail,"test@test.com",all = F)
+  expect_match(mail,"Content-Type: text/html",all = F)
 })
 
+test_that("send_email sends attachments", {
+  stub(send_email, "config::get", mock("test@test.com", list(host.name = "blah"), cycle = TRUE))
+  filename <- tempfile()
+  writeLines("Hi!!!! 😊✨",filename)
+  mail <- send_email(engine="debug","This is a test email",attachments = c(attachment.txt = filename))
+  expect_match(mail,"This is a test email",all = F)
+  expect_match(mail,"test@test.com",all = F)
+  expect_match(mail,"Content-Type: text/html",all = F)
+  expect_match(mail,base64enc::base64encode(charToRaw("Hi!!!! 😊✨\r\n")),all = F)
+})
 
 
 # send_xlsx ---------------------------------------------------------------
@@ -52,21 +61,16 @@ test_that("send_xlsx calls send_email with the specified args", {
   a_table <- data.table(x = seq(1000))
 
   send_xlsx(a_table)
-  expect_equal(mock_args(send_email)[[1]][["subject"]],paste("a_table",Sys.Date()))
-  expect_equal(mock_args(send_email)[[1]][["body"]],paste("Sent by",Sys.info()["nodename"]))
-  expect_equal(mock_args(send_email)[[1]][["emails"]],"ssyzygy@bam.org")
-  expect_equal(mock_args(send_email)[[1]][["html"]],TRUE)
-  expect_match(mock_args(send_email)[[1]][["attach.files"]],"\\.xlsx$")
-  expect_match(mock_args(send_email)[[1]][["file.names"]],paste0("a_table_",Sys.Date(),".xlsx"))
+  expect_equal(mock_args(send_email)[[1]]$subject,paste("a_table",Sys.Date()))
+  expect_equal(mock_args(send_email)[[1]]$body,paste("Sent by",Sys.info()["nodename"]))
+  expect_equal(mock_args(send_email)[[1]]$emails,"ssyzygy@bam.org")
+  expect_match(names(mock_args(send_email)[[1]]$attachments),paste0("a_table_",Sys.Date(),".xlsx"))
 
 
   send_xlsx(a_table, subject = "subject", body = "body", emails = "me@me.com",
             basename = "b_table")
-  expect_equal(mock_args(send_email)[[2]][["subject"]],"subject")
-  expect_equal(mock_args(send_email)[[2]][["body"]],"body")
-  expect_equal(mock_args(send_email)[[2]][["emails"]],"me@me.com")
-  expect_equal(mock_args(send_email)[[2]][["html"]],TRUE)
-  expect_match(mock_args(send_email)[[2]][["attach.files"]],"\\.xlsx$")
-  expect_match(mock_args(send_email)[[2]][["file.names"]],paste0("b_table_",Sys.Date(),".xlsx"))
-
+  expect_equal(mock_args(send_email)[[2]]$subject,"subject")
+  expect_equal(mock_args(send_email)[[2]]$body,"body")
+  expect_equal(mock_args(send_email)[[2]]$emails,"me@me.com")
+  expect_match(names(mock_args(send_email)[[2]]$attachments),paste0("b_table_",Sys.Date(),".xlsx"))
 })
