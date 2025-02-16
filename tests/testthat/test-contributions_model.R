87d3824732972fea6220ad25fe41d5ff14b1342b
Sky Syzygy <ssyzygy@bam.org>
2025-02-16 16:31:15 -0500

    Fix broken tests and speed up output tests

--- tests/testthat/test-contributions_model.R
+++ tests/testthat/test-contributions_model.R
@@ -1,7 +1,6 @@
 withr::local_package("checkmate")
 withr::local_package("mockery")
 
-
 # contributions_dataset ------------------------------------------------
 
 test_that("contributions_dataset reads from cache and adds an event indicator", {
@@ -11,7 +10,8 @@ test_that("contributions_dataset reads from cache and adds an event indicator", 
     group_customer_no = 1,
     event_type = c("Ticket","Contribution"),
     contribution_amt = 50,
-    timestamp = Sys.Date()+c(-60,-.001)),
+    timestamp = Sys.Date()+c(-60,-.001),
+    rowid = 1:2),
     arrow::arrow_table(date = lubridate::POSIXct())
   )
 
@@ -26,10 +26,11 @@ test_that("contributions_dataset reads from cache and adds an event indicator", 
 
 })
 
-test_that("contributions_dataset rebuilds all data when rebuild_dataset = T", {
+test_that("contributions_dataset rebuilds all data when rebuild_dataset = TRUE", {
   tessilake::local_cache_dirs()
 
   read_cache <- mock(arrow::arrow_table(
+    rowid = 1:2,
     group_customer_no = 1,
     event_type = c("Ticket","Contribution"),
     contribution_amt = 50,
@@ -41,6 +42,7 @@ test_that("contributions_dataset rebuilds all data when rebuild_dataset = T", {
   contributions_dataset()
 
   stub(contributions_dataset, "read_cache", mock(arrow::arrow_table(
+      rowid = 1:4,
       group_customer_no = 1:4,
       event_type = rep(c("Ticket","Contribution"),2),
       contribution_amt = 50,
@@ -52,14 +54,15 @@ test_that("contributions_dataset rebuilds all data when rebuild_dataset = T", {
   contributions_dataset(rebuild_dataset = T)
 
   expect_length(mock_args(dataset_chunk_write),1)
-  expect_equal(mock_args(dataset_chunk_write)[[1]][["partition"]], year(Sys.Date()))
+  expect_equal(mock_args(dataset_chunk_write)[[1]][["partition"]], year(Sys.Date()-60))
 
 })
 
-test_that("contributions_dataset only appends data when rebuild_dataset = TRUE", {
+test_that("contributions_dataset only appends data when rebuild_dataset = NULL", {
   tessilake::local_cache_dirs()
 
   stub(contributions_dataset, "read_cache", mock(arrow::arrow_table(
+    rowid = 1:2,
     group_customer_no = 1,
     event_type = c("Ticket","Contribution"),
     contribution_amt = 50,
@@ -72,6 +75,7 @@ test_that("contributions_dataset only appends data when rebuild_dataset = TRUE",
   stub(contributions_dataset, "read_cache", mock(
     read_cache("dataset","contributions_model"),
     arrow::arrow_table(
+      rowid = 1:4,
       group_customer_no = 1:4,
       event_type = rep(c("Ticket","Contribution"),2),
       contribution_amt = 50,
@@ -86,14 +90,15 @@ test_that("contributions_dataset only appends data when rebuild_dataset = TRUE",
 
   contributions_dataset(until = Inf)
   expect_length(mock_args(dataset_chunk_write),1)
-  expect_equal(mock_args(dataset_chunk_write)[[1]][["partition"]], year(Sys.Date())+1)
+  expect_equal(mock_args(dataset_chunk_write)[[1]][["partition"]], year(Sys.Date()+365))
 
 })
 
-test_that("contributions_dataset only reads data when rebuild_dataset = F", {
+test_that("contributions_dataset only reads data when rebuild_dataset = FALSE", {
   tessilake::local_cache_dirs()
 
   stub(contributions_dataset, "read_cache", mock(arrow::arrow_table(
+    rowid = 1:2,
     group_customer_no = 1,
     event_type = c("Ticket","Contribution"),
     contribution_amt = 50,
@@ -141,9 +146,19 @@ test_that("read.contributions_model creates a valid mlr3 validation task", {
 # train.contributions_model -----------------------------------------------
 tessilake::local_cache_dirs()
 test_that("train.contributions_model successfully trains a model", {
+  dataset <- arrow::read_parquet(rprojroot::find_testthat_root_file("test-contributions_model.parquet"), as_data_frame = F)
+  stub(read.contributions_model, "contributions_dataset",\(...) {dataset})
+
+  stub(read.contributions_model,"cache_exists_any",TRUE)
+
+  dates <- dataset %>% select(date) %>% collect
+
+  .model <- read(contributions_model, predict_since = median(dates$date),
+                 downsample_read = 1, rebuild_dataset = F)
+
   future::plan("sequential")
 
-  suppressWarnings(model <<- train(model, num_trees = 16))
+  suppressWarnings(model <<- train(.model, num_trees = 16, downsample_train = 1))
 
   expect_class(model$model, "Learner")
 
@@ -160,27 +175,19 @@ test_that("predict.contributions_model successfully predicts new data", {
 
 })
 
-
 # output.contributions_model ---------------------------------------------
 
 test_that("output.contributions_model successfully interprets the model", {
+  dataset <- \(...) {arrow::read_parquet(rprojroot::find_testthat_root_file("test-contributions_model.parquet"), as_data_frame = F)}
+  stub(output, "contributions_dataset",dataset,2)
+  stub(read.contributions_model, "contributions_dataset",dataset,2)
 
   withr::local_options(future.globals.maxSize = 1024^3)
 
-  # predict the whole thing
-  model$predictions <-
-    cbind(as.data.table(model$model$predict(model$task)),
-          model$task$data(cols = c("I","group_customer_no","date")))
-
-  # dataset
-  d <- arrow::read_parquet(rprojroot::find_testthat_root_file("test-contributions_model.parquet"),
-                           as_data_frame = F) %>% collect %>% setDT
-
-  model$dataset <- d[seq_len(.N)<.01*.N]
-
   dir.create(cache_primary_path("","contributions_model"))
 
-  suppressWarnings(output(model,downsample_output = 1))
+  read(model)
+  output(model, features = c("contribution_amt","contribution_timestamp_max"), n.repetitions = 1, n_features = 5, n_top = 5)
 
   pdf_filename <- cache_primary_path("contributions_model.pdf","contributions_model")
   exp_filename <- cache_primary_path("shapley.Rds","contributions_model")
@@ -188,7 +195,8 @@ test_that("output.contributions_model successfully interprets the model", {
   expect_file_exists(exp_filename)
 
   explanations <- readRDS(exp_filename)
-  expect_equal(nrow(explanations),model$predictions[model$dataset,on="I"][prob.TRUE>.75,.N])
+  expect_data_table(explanations)
+  expect_names(colnames(explanations), must.include = "explanation")
 
 })
 
